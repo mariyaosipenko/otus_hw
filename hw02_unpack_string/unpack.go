d6e3d9174f8c4448e260d50c122a650a1fdef3e0
Mariya Osipenko <mariyaosipenko@gmail.com>
2023-08-09 14:49:58 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,74 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var resultString strings.Builder
+
+	if len(input) == 0 {
+		return resultString.String(), nil
+	}
+
+	inputRune := []rune(input)
+
+	// если первый символ цифра, то это ошибка
+	if _, errCurrent := strconv.Atoi(string(inputRune[0])); errCurrent == nil {
+		return "", ErrInvalidString
+	}
+
+	// если последний символ слэш, то это ошибка
+	if inputRune[len(inputRune)-1] == 92 {
+		return "", ErrInvalidString
+	}
+
+	var i int
+	for i = 0; i < len(inputRune)-1; i++ {
+		var simvol string
+		multi := 1
+
+		// проверяем, может быть следующий символ цифра
+		nextRuneAsNumber, errNext := strconv.Atoi(string(inputRune[i+1]))
+
+		if inputRune[i] == 92 {
+			// экранировать можем только слэш или цифру
+			if inputRune[i+1] != 92 && errNext != nil {
+				return "", ErrInvalidString
+			}
+			// берем следующие символы
+			simvol = string(inputRune[i+1])
+			i++
+			if i < len(inputRune)-1 {
+				nextRuneAsNumber, errNext = strconv.Atoi(string(inputRune[i+1]))
+			}
+		} else {
+			simvol = string(inputRune[i])
+		}
+
+		// следующий символ цифра
+		if errNext == nil && i < len(inputRune)-1 {
+			// но не двузначное число
+			if i < len(inputRune)-2 {
+				if _, errNextNext := strconv.Atoi(string(inputRune[i+2])); errNextNext == nil {
+					return "", ErrInvalidString
+				}
+			}
+			multi = nextRuneAsNumber
+			i++
+		}
+
+		resultString.WriteString(strings.Repeat(simvol, multi))
+	}
+
+	// последний символ просто дописываем в итоговую строку
+	if i == len(inputRune)-1 {
+		resultString.WriteRune(inputRune[i])
+		return resultString.String(), nil
+	}
+
+	return resultString.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,13 +16,12 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
-
 	for _, tc := range tests {
 		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
@@ -34,7 +33,13 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{
+		"3abc",
+		"45",
+		"aaa10b",
+		`qw\ne`,
+		`qwne\`,
+	}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
