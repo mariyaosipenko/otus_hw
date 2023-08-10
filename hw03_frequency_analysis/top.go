c331a31bb5441198a3be2cf1246bb776abc27917
Mariya Osipenko <mariyaosipenko@gmail.com>
2023-08-10 15:50:31 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,35 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(str string) []string {
+	w := strings.Fields(str)
+	if len(w) == 0 {
+		return nil
+	}
+
+	wordFrequency := make(map[string]int, len(w))
+	for _, v := range w {
+		pv := strings.ToLower(strings.Trim(v, "!¡,.'-"))
+		if len(pv) > 0 {
+			wordFrequency[pv]++
+		}
+	}
+	keys := make([]string, 0, len(wordFrequency))
+
+	for key := range wordFrequency {
+		keys = append(keys, key)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		if wordFrequency[keys[i]] == wordFrequency[keys[j]] {
+			return keys[i] < keys[j]
+		}
+		return wordFrequency[keys[i]] > wordFrequency[keys[j]]
+	})
+
+	return keys[:10]
 }
